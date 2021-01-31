170211c072fc2c1ca391d0b694f01cdfea0f1300
Tommy Tang <ziyet3@linux-a2.ews.illinois.edu>
2021-01-30 20:19:42 -0600

    My Art is notactually an art

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -8,4 +8,27 @@
 #include "HSLAPixel.h"
 
 namespace cs225 {
+    HSLAPixel::HSLAPixel()
+    {
+        h = 0;
+        l = 1.0;
+        s = 0;
+        a = 1.0;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance)
+    {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1.0;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha)
+    {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    }
 }
